67453718fbaf8a37bc27585c4def90b6161192b8
Umar22222 <asadovumar222@gmail.com>
2024-08-30 03:23:37 +0500

    .

---
+++ js/app.js
@@ -0,0 +1,537 @@
+{
+    // 1 - misol
+    // let a = prompt("kunni kiriting")
+
+    // switch(a){
+    //     case 0:
+    //         console.log(`${a} - kun  yo'q`);
+    //         break
+    //     case 1:
+    //         console.log(`${a} - kun  Dushanba`);
+    //         break
+    //     case 2:
+    //         console.log(`${a} - kun  Seshanba`);
+    //         break
+    //     case 3:
+    //         console.log(`${a} - kun  CHorshanba`);
+    //         break
+    //     case 4:
+    //         console.log(`${a} - kun  Payshanba`);
+    //         break
+    //     case 5:
+    //         console.log(`${a} - kun  Juma`);
+    //         break
+    //     case 6:
+    //         console.log(`${a} - kun  SHanba`);
+    //         break
+    //     case 7:
+    //         console.log(`${a} - kun  Yakshanba`);
+    //        default:
+    //         console.log(`1 dan 7 gacha yoz`);
+             
+    // }
+}
+
+{
+    // 2 - misol
+    // let a = +prompt("1 dam 5 gacha bohono yozin")
+
+    // switch(a){
+    //     case 1:
+    //         console.log(`${a} - Yomon`);
+    //         break
+    //     case 2:
+    //         console.log(`${a} - Qoniqarsiz`);
+    //         break
+    //     case 3:
+    //         console.log(`${a} - Qoniqarli`);
+    //         break
+    //     case 4:
+    //         console.log(`${a} - Yahshi`);
+    //         break
+    //     case 5:
+    //         console.log(`${a} - A'lo`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 3 - misol
+    // let a = +prompt("oyni raqamini kiriting")
+
+    // switch(a){
+    //     case 1:
+    //     case 2:
+    //     case 12:
+    //         console.log(`Qish`);
+    //         break
+    //     case 3:
+    //     case 4:
+    //     case 5:
+    //         console.log(`Bahor`);
+    //         break
+    //     case 6:
+    //     case 7:
+    //     case 8:
+    //         console.log(`Yoz`);
+    //         break
+    //     case 9:
+    //     case 10:
+    //     case 11:
+    //         console.log(`Kuz`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 4 - misol
+    // let a = +prompt("oyni kiriting yoki raqamini ")
+    // switch(a){
+    //     case 1:
+    //     case `Yanvar`:
+    //         console.log(`Yanvar: 31 kun`);
+    //         break
+    //     case 2:
+    //     case `Fevral`:
+    //         console.log(`Fevral: 28 kun (kabisa yillarida 29 kun)`);
+    //         break
+    //     case 3:
+    //         case `Mart`:
+    //         console.log(`Mart: 31 kun`);
+    //         break
+    //     case 4:
+    //     case `Aprel`:
+    //         console.log(`Aprel: 30 kun`);
+    //         break
+    //     case 5:
+    //     case `May`:
+    //         console.log(`May: 31 kun`);
+    //         break
+    //     case 6:
+    //     case `Iyun`:
+    //         console.log(`Iyun: 30 kun`);
+    //         break
+    //     case 7:
+    //     case `Iyul`:
+    //         console.log(`Iyul: 31 kun`);
+    //         break
+    //     case 8:
+    //     case `Avgust`:
+    //         console.log(`Avgust: 31 kun`);
+    //         break
+    //     case 9:
+    //     case `Sentabr`:
+    //         console.log(`Sentabr: 30 kun`);
+    //         break
+    //     case 10:
+    //     case `Oktabr`:
+    //         console.log(`Oktabr: 31 kun`);
+    //         break
+    //     case 11:
+    //     case `Noyabr`:
+    //         console.log(`Noyabr: 30 kun`);
+    //         break
+    //     case 12:
+    //     case `Dekabr`:
+    //         console.log(`Dekabr: 31 kun`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 5 - misol
+    // let a = +prompt("sonlarni kiriting")
+    // let c = prompt("amalni kiriting")
+    // let b = +prompt("sonlarni kiriting")
+
+
+    // switch(c){
+    //     case "+":
+    //     console.log(a + b);
+    //     break
+    //     case "-":
+    //     console.log(a - b);
+    //     break
+    //     case "*":
+    //     console.log(a * b);
+    //     break
+    //     case "/":
+    //     console.log(a / b);
+    //     break
+    //     default:
+    //         console.log(`Xato`);
+            
+        
+    // }
+}
+
+{
+    // 6 - misol
+    // let a = +prompt("Uzunlik tanlang: 1 - Desimetr 2 - Kilometr 3- Metr 4 - Millimetr 5 - Santimetr");
+    // let b = +prompt("Kesmaning uzunligini kiriting:");
+    // let c;
+
+    // switch (a) {
+    //     case 1:
+    //         console.log(c = b / 10);
+            
+    //         break;
+    //     case 2:
+    //         console.log(c = b * 1000);
+            
+    //         break;
+    //     case 3:
+    //         console.log(c = b);
+            
+    //         break;
+    //     case 4:
+    //         console.log(c = b / 1000);
+            
+    //         break;
+    //     case 5:
+    //         console.log(c = b / 100);
+            
+    //         break;
+    //     default:
+    //         console.log(`Xato`);
+            
+    // }
+
+    // if (typeof c === "string") {
+    //     console.log(c);
+    // } else {
+    //     console.log(" Kesmaning uzunligi " + c + " metr ");
+    // }
+}
+
+{
+    // 7 - misol
+    // let a = +prompt("sonni kiriting") 
+    // let b = +prompt("sonni kiriting")
+    // let c
+
+    // switch (a) {
+    //     case 1:
+    //         c = b
+    //         console.log(`${b} kilogram ${c} kilogramga teng`);
+    //         break;
+    //     case 2:
+    //         c = b * 1e-6
+    //         console.log(`${b} miligram ${c} kilogramga teng`);
+    //         break
+    //     case 3:
+    //         c = b * 0.001
+    //         console.log(`${b} gram ${c} kilogramga teng`);
+    //         break
+    //     case 4:
+    //         c = b * 1000
+    //         console.log(`${b} tonna ${c} kilogramga teng`);
+    //         break
+    //     case 5:
+    //         c = b * 100
+    //         console.log(`${b} sentner ${c} kilogramga teng`);
+    //         break
+    //     default:
+    //         console.log(`Xato`);
+            
+    // }
+}
+
+{
+    // 8 - misol
+//   let a = +prompt("kunni kiriting");
+//   let b = +prompt("kunni kiriting");
+
+//   if (a > 0 && a < 32) {
+//     switch (b) {
+//       case 1:
+//         console.log(`${a} - Yanvar , yilning ${a} = kuni`);
+
+//         break;
+//       case 2:
+//         if (a != 29 && a != 30 && a != 31) {
+//           console.log(`${a} - Fevral,Yilning ${31 + a} = kuni`);
+//         } else console.log(`bu oyda unaqa kun yoq`);
+
+//         break;
+//       case 3:
+//         console.log(`${a} - Mart, Yilning ${31 + 28 + a} = kuni`);
+
+//         break;
+//       case 4:
+//         if (a != 31)
+//           console.log(`${a} - Aprel, Yilning ${31 + 28 + 31 + a} = kuni`);
+//         else console.log(`Xato`);
+//         break;
+//       case 5:
+//         console.log(`${a} - May, Yilning ${31 + 28 + 31 + 30 + a} = kuni`);
+
+//         break;
+
+//       case 6:
+//         if (a != 31)
+//           console.log(
+//             `${a} - Iyun, Yilning ${31 + 28 + 31 + 30 + 31 + a} = kuni`
+//           );
+//         else console.log(`Xato`);
+//         break;
+//       case 7:
+//         console.log(
+//           `${a} - Iyul, Yilning ${31 + 28 + 31 + 30 + 31 + 30 + a} = kuni`
+//         );
+
+//         break;
+//       case 8:
+//         console.log(
+//           `${a} - Avgust, Yilning ${31 + 28 + 31 + 30 + 31 + 30 + 31 + a} = kuni`
+//         );
+
+//         break;
+//       case 9:
+//         if (a != 31)
+//           console.log(
+//             `${a} - Sentabr, Yilning ${
+//               31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + a
+//             } kuni`
+//           );
+//         else console.log(`bu xato`);
+
+//         break;
+//       case 10:
+//         console.log(
+//           `${a} - Oktabr, Yilning ${
+//             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + a
+//           } kuni`
+//         );
+
+//         break;
+//       case 11:
+//         if (a != 31)
+//           console.log(
+//             `${a} - Noyabr, Yilning ${
+//               31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + a
+//             } kuni`
+//           );
+//         else console.log(`bu xato`);
+//         break;
+//       case 12:
+//         console.log(
+//           `${a} - dekabr, Yilning ${
+//             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + a
+//           } kuni`
+//         );
+
+//         break;
+
+//       default:
+//         console.log(`xato `);
+//         break;
+//     }
+//   } else console.log(`Xato`);
+}
+
+{
+    // 9 - misol
+    // let a = +prompt("kunni kiriting");
+    // let b = +prompt("kunni kiriting");
+    // if (a > 0 && a < 32) {
+    //   switch (b) {
+    //     case 1:
+    //       if (a === 31) console.log(`Ertaga 1 - Fevral yilning 32 - kuni`);
+    //       else console.log(`Ertaga ${a + 1} - yanvar,yilning ${a + 1} kuni`);
+  
+    //       break;
+    //     case 2:
+    //       if (a != 30 && a != 31 && a != 29) {
+    //         if (a === 28) {
+    //           console.log(`Ertaga 1- mart,Yilning ${31 + 28 + 1} - kuni`);
+    //         } else {
+    //           console.log(
+    //             `Ertaga ${a + 1} - Fevral,yilning ${31 + a + 1} - kuni`
+    //           );
+    //         }
+    //       } else {
+    //         console.log(`Xato`);
+    //       }
+    //       break;
+    //     case 3:
+    //       if (a === 31)
+    //         console.log(`Ertaga 1 - aprel,yilning ${31 + 28 + 31 + 1} - kuni`);
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - mart , yolning ${31 + 28 + a + 1} - kuni`
+    //         );
+    //       break;
+    //     case 4:
+    //       if (a === 30)
+    //         console.log(`Ertaga 1 - may , yilning ${31 + 28 + 31 + 30 + 1} - kuni`);
+    //       else if (a !== 31) {
+    //         console.log(
+    //           `Ertaga ${a + 1} - aprel , yilning ${31 + 28 + 31 + a + 1} - kuni`
+    //         );
+    //       } else console.log(`Xato`);
+    //       break;
+    //     case 5:
+    //       if (a === 31)
+    //         console.log(
+    //           `Ertaga 1 - iyun,yilning ${31 + 28 + 31 + 30 + 31 + 1} - kuni`
+    //         );
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - may , yilning ${
+    //             31 + 28 + 31 + 30 + a + 1
+    //           } - kuni`
+    //         );
+    //       break;
+    //     case 6:
+    //       if (a === 30)
+    //         console.log(
+    //           `Ertaga 1 - iyul,yilning ${31 + 28 + 31 + 30 + 31 + 30 + 1} - kuni`
+    //         );
+    //       else if (a !== 31) {
+    //         console.log(
+    //           `Ertaga ${a + 1} - iyun , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + a + 1
+    //           } - kuni`
+    //         );
+    //       } else console.log(`Xato`);
+    //       break;
+    //     case 7:
+    //       if (a === 31)
+    //         console.log(
+    //           `Ertaga 1 - Avgust,yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 1
+    //           } - kuni`
+    //         );
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - iyul , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + a + 1
+    //           } - kuni`
+    //         );
+    //       break;
+    //     case 8:
+    //       if (a === 31)
+    //         console.log(
+    //           `Ertaga 1 - sentabr,yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 1
+    //           } - kuni`
+    //         );
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - avgust , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + a + 1
+    //           } - kuni`
+    //         );
+    //       break;
+    //     case 9:
+    //       if (a === 30)
+    //         console.log(
+    //           `Ertaga 1 - oktabr,yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 1
+    //           } - kuni`
+    //         );
+    //       else if (a !== 31) {
+    //         console.log(
+    //           `Ertaga ${a + 1} - sentabr , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + a + 1
+    //           } - kuni`
+    //         );
+    //       } else console.log(`Xato`);
+    //       break;
+    //     case 10:
+    //       if (a === 31)
+    //         console.log(
+    //           `Ertaga 1 - Noyabr,yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 1
+    //           } - kuni`
+    //         );
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - oktabr , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + a + 1
+    //           } - kuni`
+    //         );
+    //       break;
+    //     case 11:
+    //       if (a === 30)
+    //         console.log(
+    //           `Ertaga 1 - Dekabr,yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 1
+    //           } - kuni`
+    //         );
+    //       else if (a !== 31) {
+    //         console.log(
+    //           `Ertaga ${a + 1} - noyabr , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + a + 1
+    //           } - kuni`
+    //         );
+    //       } else console.log(`Xato`);
+    //       break;
+    //     case 12:
+    //       if (a === 31) console.log(`Bugun 31- dekabr yilning ohirgi 365 - kuni`);
+    //       else
+    //         console.log(
+    //           `Ertaga ${a + 1} - dekabr , yilning ${
+    //             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + a + 1
+    //           } - kuni`
+    //         );
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // } else console.log(`Xato`);
+  }
+
+  {
+    // 10 - misol
+    // function robotHolati(yunalish, komanda) {
+    //     switch (komanda) {
+    //         case 0:
+    //             return yunalish;
+    //         case 1:
+    //             switch (yunalish) {
+    //                 case 's':
+    //                     return 'g';
+    //                 case 'j':
+    //                     return 'q';
+    //                 case 'q':
+    //                     return 's';
+    //                 case 'g':
+    //                     return 'j';
+    //                 default:
+    //                     return "Noto'g'ri yo'nalish!";
+    //             }
+    //         case 2:
+    //             switch (yunalish) {
+    //                 case 's':
+    //                     return 'q';
+    //                 case 'j':
+    //                     return 'g';
+    //                 case 'q':
+    //                     return 'j';
+    //                 case 'g':
+    //                     return 's';
+    //                 default:
+    //                     return "Noto'g'ri yo'nalish!";
+    //             }
+    //         default:
+    //             return "Noto'g'ri komanda!";
+    //     }
+    // }
+    
+
+    // let yunalish = 's';
+    // let komanda = 1;
+    // let yangiYunalish = robotHolati(yunalish, komanda);
+    // console.log("Yangi yo'nalish:", yangiYunalish);
+  }
