0f03acc835b88f42d6955cde5475e21f5e4d442c
Umar22222 <asadovumar222@gmail.com>
2024-08-30 18:44:05 +0500

    .

---
+++ homeworkjs/js/app.js
@@ -0,0 +1,179 @@
+{
+    // 1 - misol
+    // let a = prompt("kunni kiriting")
+
+    // switch(a){
+    //     case 0:
+    //         console.log(`${a} - kun  yo'q`);
+    //         break
+    //     case 1:
+    //         console.log(`${a} - kun  Dushanba`);
+    //         break
+    //     case 2:
+    //         console.log(`${a} - kun  Seshanba`);
+    //         break
+    //     case 3:
+    //         console.log(`${a} - kun  CHorshanba`);
+    //         break
+    //     case 4:
+    //         console.log(`${a} - kun  Payshanba`);
+    //         break
+    //     case 5:
+    //         console.log(`${a} - kun  Juma`);
+    //         break
+    //     case 6:
+    //         console.log(`${a} - kun  SHanba`);
+    //         break
+    //     case 7:
+    //         console.log(`${a} - kun  Yakshanba`);
+    //        default:
+    //         console.log(`1 dan 7 gacha yoz`);
+             
+    // }
+}
+
+{
+    // 2 - misol
+    // let a = +prompt("1 dam 5 gacha bohono yozin")
+
+    // switch(a){
+    //     case 1:
+    //         console.log(`${a} - Yomon`);
+    //         break
+    //     case 2:
+    //         console.log(`${a} - Qoniqarsiz`);
+    //         break
+    //     case 3:
+    //         console.log(`${a} - Qoniqarli`);
+    //         break
+    //     case 4:
+    //         console.log(`${a} - Yahshi`);
+    //         break
+    //     case 5:
+    //         console.log(`${a} - A'lo`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 3 - misol
+    // let a = +prompt("oyni raqamini kiriting")
+
+    // switch(a){
+    //     case 1:
+    //     case 2:
+    //     case 12:
+    //         console.log(`Qish`);
+    //         break
+    //     case 3:
+    //     case 4:
+    //     case 5:
+    //         console.log(`Bahor`);
+    //         break
+    //     case 6:
+    //     case 7:
+    //     case 8:
+    //         console.log(`Yoz`);
+    //         break
+    //     case 9:
+    //     case 10:
+    //     case 11:
+    //         console.log(`Kuz`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 4 - misol
+    // let a = +prompt("oyni kiriting yoki raqamini ")
+    // switch(a){
+    //     case 1:
+    //     case `Yanvar`:
+    //         console.log(`Yanvar: 31 kun`);
+    //         break
+    //     case 2:
+    //     case `Fevral`:
+    //         console.log(`Fevral: 28 kun (kabisa yillarida 29 kun)`);
+    //         break
+    //     case 3:
+    //         case `Mart`:
+    //         console.log(`Mart: 31 kun`);
+    //         break
+    //     case 4:
+    //     case `Aprel`:
+    //         console.log(`Aprel: 30 kun`);
+    //         break
+    //     case 5:
+    //     case `May`:
+    //         console.log(`May: 31 kun`);
+    //         break
+    //     case 6:
+    //     case `Iyun`:
+    //         console.log(`Iyun: 30 kun`);
+    //         break
+    //     case 7:
+    //     case `Iyul`:
+    //         console.log(`Iyul: 31 kun`);
+    //         break
+    //     case 8:
+    //     case `Avgust`:
+    //         console.log(`Avgust: 31 kun`);
+    //         break
+    //     case 9:
+    //     case `Sentabr`:
+    //         console.log(`Sentabr: 30 kun`);
+    //         break
+    //     case 10:
+    //     case `Oktabr`:
+    //         console.log(`Oktabr: 31 kun`);
+    //         break
+    //     case 11:
+    //     case `Noyabr`:
+    //         console.log(`Noyabr: 30 kun`);
+    //         break
+    //     case 12:
+    //     case `Dekabr`:
+    //         console.log(`Dekabr: 31 kun`);
+    //         break
+    //         default:
+    //             console.log(`Xato`);
+                
+    // }
+}
+
+{
+    // 5 - misol
+    // let a = +prompt("sonlarni kiriting")
+    // let c = prompt("amalni kiriting")
+    // let b = +prompt("sonlarni kiriting")
+
+
+    // switch(c){
+    //     case "+":
+    //     console.log(a + b);
+    //     break
+    //     case "-":
+    //     console.log(a - b);
+    //     break
+    //     case "*":
+    //     console.log(a * b);
+    //     break
+    //     case "/":
+    //     console.log(a / b);
+    //     break
+    //     default:
+    //         console.log(`Xato`);
+            
+        
+    // }
+}
+
+{
+
+  }
